f7fee58f2d202f48dd278b5fc8f3acd2d30c6b68
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2024-12-26 11:40:58 +0100

    Refactored upup cmd to class form.
    The upup cmd was written in a deprecated style but now it uses a class that inherits from AbstractCommand.

--- cmd/upup.rb
+++ cmd/upup.rb
@@ -1,34 +1,27 @@
-require "cli/parser"
-
 module Homebrew
-  module_function
+  module Cmd
+    class Upup < AbstractCommand
+      cmd_args do
+        switch "-c", "--cleanup",
+          description: "Calls brew cleanup after the last step"
+        switch "-q", "--quiet",
+          description: "Runs quietly with no output unless something goes wrong"
+      end
 
-  def upup_args
-    Homebrew::CLI::Parser.new do
-      usage_banner <<~EOS
-      `upup` [`--cleanup`] [`--quiet`]
-      EOS
-      switch "-c", "--cleanup",
-        description: "Calls brew cleanup after the last step"
-      switch "-q", "--quiet",
-        description: "Runs quietly with no output unless something goes wrong"
-    end
-  end
+      def callbrew(command, args)
+        puts "brew #{command}" unless args.quiet?
+        system "brew", command
+        abort("brew #{command} failed") unless $CHILD_STATUS.success?
+      end
 
-  def callbrew(command, args)
-    puts "brew #{command}" unless args.quiet?
-    system "brew", command
-    abort("brew #{command} failed") unless $CHILD_STATUS.success?
-  end
+      def run
+        commands = %w(update upgrade)
+        commands << "cleanup" if args.cleanup?
 
-  def upup
-    args = upup_args.parse
-
-    commands = %w(update upgrade)
-    commands << "cleanup" if args.cleanup?
-
-    commands.each do |command|
-      callbrew(command, args)
+        commands.each do |command|
+          callbrew(command, args)
+        end
+      end
     end
   end
 end
